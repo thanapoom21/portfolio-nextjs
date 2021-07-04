eb0d9b1060cc6fed97be4538b25d2d5df591bfcb
thanapoom21 <thanapoom.p.music@gmail.com>
2021-07-03 18:14:43 -0700

    update content, email and style

--- pages/contact.js
+++ pages/contact.js
@@ -10,7 +10,26 @@ export default function Contact() {
       </Head>
       <section className={utilStyles.headingMd}>
         <h1 className={utilStyles.superBoldText4rem}>Contact</h1>
+        <p className={utilStyles.superBoldText3rem}>
+          I'm available for freelance work. Drop me a line if you'd like to
+          chat.
+        </p>
+        <p className={utilStyles.headingXl}>
+          <a href="mailto:thanapoom.p.music@gmail.com?Subject=Web Development">
+            thanapoom.p.music@gmail.com
+          </a>
+        </p>
+
+        {repeatIt(3, <div>&#9660;</div>)}
       </section>
     </Layout>
   );
 }
+
+const repeatIt = (numberOfTimes, thingsToRepeat) => {
+  let element = [];
+  for (let index = 0; index < numberOfTimes; index++) {
+    element.push(thingsToRepeat);
+  }
+  return element;
+};
