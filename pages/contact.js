ea1ad01983237867f2c5c4ed3b1c5b783607fed2
thanapoom21 <thanapoom.p.music@gmail.com>
2021-07-05 17:10:50 -0700

    update margin-bottom, create AnimatedText and pass in the text for animation

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,13 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
+import { Box, Button } from "@chakra-ui/react";
+import { EmailIcon } from "@chakra-ui/icons";
+import { SplitText } from "../components/SplitText";
+import { AnimatePresence, motion } from "framer-motion";
 import utilStyles from "../styles/utils.module.css";
 
 export default function Contact() {
+  const textToAnimate = `I am available for freelance work. Drop me a line if you would like to chat. 😄`;
   return (
     <Layout>
       <Head>
@@ -10,26 +15,75 @@ export default function Contact() {
       </Head>
       <section className={utilStyles.headingMd}>
         <h1 className={utilStyles.superBoldText4rem}>Contact</h1>
-        <p className={utilStyles.superBoldText3rem}>
-          I'm available for freelance work. Drop me a line if you'd like to
-          chat.
-        </p>
-        <p className={utilStyles.headingXl}>
-          <a href="mailto:thanapoom.p.music@gmail.com?Subject=Web Development">
-            thanapoom.p.music@gmail.com
-          </a>
-        </p>
-
-        {repeatIt(3, <div>&#9660;</div>)}
+        <Box mt={10}>
+          <AnimatedText>{textToAnimate}</AnimatedText>
+          <Button
+            my={10}
+            leftIcon={<EmailIcon />}
+            colorScheme="blackAlpha"
+            variant="outline"
+          >
+            <a href="mailto:thanapoom.p.music@gmail.com?Subject=Web Development">
+              Email me
+            </a>
+          </Button>
+          {/* <Repeat numberOfTimes={5}>
+            <div>&#9660;</div>
+          </Repeat> */}
+        </Box>
       </section>
     </Layout>
   );
 }
 
-const repeatIt = (numberOfTimes, thingsToRepeat) => {
-  let element = [];
-  for (let index = 0; index < numberOfTimes; index++) {
-    element.push(thingsToRepeat);
-  }
-  return element;
-};
+// /**
+//  *
+//  * @param {Number} numberOfTimes
+//  * @param {JSX} thingsToRepeat
+//  * @returns the number of array of element to be repeated. For experiment purposes.
+//  */
+// const repeatIt = (numberOfTimes, thingsToRepeat) => {
+//   let element = [];
+//   for (let index = 0; index < numberOfTimes; index++) {
+//     element.push(thingsToRepeat);
+//   }
+//   return element;
+// };
+
+// const Repeat = ({ numberOfTimes, children }) => {
+//   let element = [];
+//   for (let index = 0; index < numberOfTimes; index++) {
+//     element.push(children);
+//   }
+//   return <>{element}</>;
+// };
+
+function AnimatedText({ children }) {
+  return (
+    <div className={utilStyles.superBoldText3rem}>
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <SplitText
+            splitBy=" "
+            initial={{ y: "100%" }}
+            animate="visible"
+            variants={{
+              visible: (i) => ({
+                y: 0,
+                transition: {
+                  delay: i * 0.02,
+                },
+              }),
+            }}
+          >
+            {children}
+          </SplitText>
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
+}
