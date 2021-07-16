0e1e11fe53f854e02d98427eab2577f2ae528ccb
thanapoom21 <thanapoom.p.music@gmail.com>
2021-07-16 01:02:02 -0700

    add grid and sections for works page

--- pages/works.js
+++ pages/works.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import HeadingOne from "../components/headingOne";
+import { Box, Image } from "@chakra-ui/react";
 import utilStyles from "../styles/utils.module.css";
 
 export default function Works() {
@@ -11,6 +12,67 @@ export default function Works() {
       </Head>
       <section className={utilStyles.headingMd}>
         <HeadingOne>Works</HeadingOne>
+        <Box
+          mt={4}
+          className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-6 gap-y-10 md:gap-y-32"
+        >
+          <section>
+            <h2 className="text-2xl md:text-4xl font-bold tracking-tighter my-2">
+              First
+            </h2>
+            <div>
+              <div className="mb-5">
+                <Image
+                  src="gibbresh.png"
+                  fallbackSrc="https://via.placeholder.com/1200x600"
+                />
+              </div>
+              <h3>Something</h3>
+            </div>
+          </section>
+          <section>
+            <h2 className="text-2xl md:text-4xl font-bold tracking-tighter my-2">
+              First
+            </h2>
+            <div>
+              <div className="mb-5">
+                <Image
+                  src="gibbresh.png"
+                  fallbackSrc="https://via.placeholder.com/1200x600"
+                />
+              </div>
+              <h3>Something</h3>
+            </div>
+          </section>
+          <section>
+            <h2 className="text-2xl md:text-4xl font-bold tracking-tighter my-2">
+              First
+            </h2>
+            <div>
+              <div className="mb-5">
+                <Image
+                  src="gibbresh.png"
+                  fallbackSrc="https://via.placeholder.com/1200x600"
+                />
+              </div>
+              <h3>Something</h3>
+            </div>
+          </section>
+          <section>
+            <h2 className="text-2xl md:text-4xl font-bold tracking-tighter my-2">
+              First
+            </h2>
+            <div>
+              <div className="mb-5">
+                <Image
+                  src="gibbresh.png"
+                  fallbackSrc="https://via.placeholder.com/1200x600"
+                />
+              </div>
+              <h3>Something</h3>
+            </div>
+          </section>
+        </Box>
       </section>
     </Layout>
   );
