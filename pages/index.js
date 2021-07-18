307128f1e007b2a184fd2c9c3a55d9d98ad35ef5
thanapoom21 <thanapoom.p.music@gmail.com>
2021-07-18 01:21:27 -0700

    change text and update styling

--- pages/index.js
+++ pages/index.js
@@ -9,24 +9,30 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p className={utilStyles.superBoldText4rem}>
+        <p
+          className={`${utilStyles.fontRoboto} text-4xl md:text-6xl font-bold md:leading-snug`}
+        >
           Hi, I'm Thanapoom and I go by Music. I'm a web developer and a
-          translator (English/Thai). 🍔 Epic cheeseburgers come in all kinds of
-          manifestations, but we want them in and around our mouth no matter
-          what. Slide those smashed patties with the gently caramelized meat fat
-          between a toasted brioche bun and pass it over. You fall in love with
-          the cheeseburger itself but the journey ain’t half bad either. They’re
-          the childhood friend that knows your highest highs and lowest lows.
-          They’ve been with you through thick and thin and they’re the best at
-          keeping secrets. Whether it’s dressed up or informal, cheeseburgers
-          have your back. 🎉
+          translator (English/Thai). I like to create graphics such as logos,
+          business cards, posters, brochures, and websites. However, I consider
+          myself as a new kid on the block in this field because there are many
+          things that I don't fully understand. I am eager to learn new things.
+          I've done freelance work for small businesses, participated in
+          startups, and collaborated with talented people to build digital
+          products for both business and consumer use. I'm naturally curious,
+          relentlessly optimistic, and gradually & constantly improving my
+          ability. 🎉
         </p>
-        <p className={utilStyles.superBoldText4rem}>
+        <p
+          className={`${utilStyles.fontRoboto} text-4xl md:text-6xl font-bold md:leading-snug`}
+        >
           Pop quiz: what’s the greatest thing to happen to your mind, body, and
           soul in recent history? A cheeseburger, obviously. Cheeseburgers know
           that what you want can also be what you need.
         </p>
-        <p className={utilStyles.superBoldText4rem}>
+        <p
+          className={`${utilStyles.fontRoboto} text-4xl md:text-6xl font-bold md:leading-snug`}
+        >
           If you feel like having a chat, feel free to drop me a line.
         </p>
       </section>
