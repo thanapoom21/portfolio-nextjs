1a94755725f11384f2562b36975b0230fa315183
thanapoom21 <thanapoom.p.music@gmail.com>
2021-09-16 17:01:07 -0700

    utilize FontAwesomeIcon component

--- pages/fcc-tutorials-projects/random-quote-machine.js
+++ pages/fcc-tutorials-projects/random-quote-machine.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const appStyle = {
   border: "solid 1px #777",
@@ -36,77 +37,77 @@ const quoteStyle = {
 };
 
 // Quotes and authors
-const quotes = [
-  {
-    quote: "Suffer the pain of discipline, or the pain of regret.",
-    author: "Batman"
-  },
-  {
-    quote: "The greatest teacher, failure is.",
-    author: "Yoda"
-  },
-  {
-    quote:
-      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
-    author: "Winston Churchill"
-  },
-  {
-    quote:
-      "If you're not prepared to be wrong, you'll never come up with anything original.",
-    author: "Ken Robinson"
-  },
-  {
-    quote: "I have not failed. I've just found 10,000 ways that won't work.",
-    author: "Thomas Edison"
-  },
-  {
-    quote:
-      "Success is stumbling from failure to failure with no loss of enthusiasm.",
-    author: "Winston Churchill"
-  },
-  {
-    quote: "Failures are finger posts on the road to achievement.",
-    author: "C. S. Lewis"
-  },
-  {
-    quote: "Discipline is the bridge between goals and accomplishment.",
-    author: "Jim Rohn"
-  },
-  {
-    quote: "I have one power: I never give up.",
-    author: "Batman"
-  },
-  {
-    quote: "Perfect is the enemy of good.",
-    author: "Voltaire"
-  },
-  {
-    quote: "I'm still learning.",
-    author: "Michelangelo"
-  },
-  {
-    quote: "Be curious, not judgmental.",
-    author: "Walt Whitman"
-  },
-  {
-    quote: "Be stubborn about your goals and flexible about your methods.",
-    author: "Unknown"
-  },
-  {
-    quote: "Anyone who stops learning is old, whether at twenty or eighty.",
-    author: "Henry Ford"
-  },
-  {
-    quote:
-      "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
-    author: "Benjamin Franklin"
-  },
-  {
-    quote:
-      "Live as if you were to die tomorrow. Learn as if you were to live forever.",
-    author: "Mahatma Ghandi"
-  }
-];
+// const quotes = [
+//   {
+//     quote: "Suffer the pain of discipline, or the pain of regret.",
+//     author: "Batman"
+//   },
+//   {
+//     quote: "The greatest teacher, failure is.",
+//     author: "Yoda"
+//   },
+//   {
+//     quote:
+//       "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+//     author: "Winston Churchill"
+//   },
+//   {
+//     quote:
+//       "If you're not prepared to be wrong, you'll never come up with anything original.",
+//     author: "Ken Robinson"
+//   },
+//   {
+//     quote: "I have not failed. I've just found 10,000 ways that won't work.",
+//     author: "Thomas Edison"
+//   },
+//   {
+//     quote:
+//       "Success is stumbling from failure to failure with no loss of enthusiasm.",
+//     author: "Winston Churchill"
+//   },
+//   {
+//     quote: "Failures are finger posts on the road to achievement.",
+//     author: "C. S. Lewis"
+//   },
+//   {
+//     quote: "Discipline is the bridge between goals and accomplishment.",
+//     author: "Jim Rohn"
+//   },
+//   {
+//     quote: "I have one power: I never give up.",
+//     author: "Batman"
+//   },
+//   {
+//     quote: "Perfect is the enemy of good.",
+//     author: "Voltaire"
+//   },
+//   {
+//     quote: "I'm still learning.",
+//     author: "Michelangelo"
+//   },
+//   {
+//     quote: "Be curious, not judgmental.",
+//     author: "Walt Whitman"
+//   },
+//   {
+//     quote: "Be stubborn about your goals and flexible about your methods.",
+//     author: "Unknown"
+//   },
+//   {
+//     quote: "Anyone who stops learning is old, whether at twenty or eighty.",
+//     author: "Henry Ford"
+//   },
+//   {
+//     quote:
+//       "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+//     author: "Benjamin Franklin"
+//   },
+//   {
+//     quote:
+//       "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+//     author: "Mahatma Ghandi"
+//   }
+// ];
 
 // Random colors;
 const colors = [
@@ -148,7 +149,7 @@ const QuoteBox = (props) => {
   return (
     <div style={quoteColor} id="text">
       <span>&ldquo;</span>
-      {props.newQuote ? props.newQuote : props.text}
+      {props.text}
       <span>&rdquo;</span>
     </div>
   );
@@ -162,7 +163,7 @@ const Author = (props) => {
 
   return (
     <div style={authColor} id="author">
-      — {props.newAuthor ? props.newAuthor : props.author}
+      — {props.author}
     </div>
   );
 };
@@ -172,25 +173,23 @@ const randomNumber = (arr) => {
 };
 
 const Twitter = (props) => {
-  const formatted =
-    "https://www.twitter.com/intent/tweet?hashtags=quotes&text=%22" +
-    props.quote.replace(" ", "%20") +
-    "%22%20—" +
-    props.author.replace(" ", "%20");
+  const formatted = `https://www.twitter.com/intent/tweet?hashtags=quotes&text=%22${props.text.replace(" ", "%20")}%22%20-${props.author.replace(" ", "%20")}`;
 
   return (
-    <a
-      id="tweet-quote"
-      target="_blank"
-      title="Tweet this quote!"
-      href={formatted}
-    >
-      <i className="fa fa-twitter fa-3x" aria-hidden="true"></i>
-    </a>
+    <div className="w-12 text-blue-400">
+      <a
+        id="tweet-quote"
+        target="_blank"
+        title="Tweet this quote!"
+        href={formatted}
+      >
+        <FontAwesomeIcon icon={['fab', 'twitter']} />
+      </a>
+    </div>
   );
 };
 
-const Button = ({ color, getQuote, children }) => {
+const QuoteBoxButton = ({ color, getQuote, children }) => {
   const [hover, setHover] = useState(false);
   let buttonStyle;
 
@@ -203,7 +202,7 @@ const Button = ({ color, getQuote, children }) => {
     })
 
   return (
-    <button
+    <div
       id="new-quote"
       style={buttonStyle}
       onClick={getQuote}
@@ -211,7 +210,7 @@ const Button = ({ color, getQuote, children }) => {
       onMouseLeave={() => setHover(false)}
     >
       {children}
-    </button>
+    </div>
   );
 }
 
@@ -219,16 +218,11 @@ class RandomMachine extends React.Component {
   constructor(props) {
     super(props);
 
-    // Random number for initial states
-    let initQuote = randomNumber(quotes);
-    let initColor = randomNumber(colors);
-    let initDarkColor = randomNumber(colorsDark);
-
     this.state = {
-      text: quotes[initQuote].quote,
-      author: quotes[initQuote].author,
-      color: colors[initColor],
-      colorDark: colorsDark[initDarkColor],
+      text: "",
+      author: "",
+      color: "",
+      colorDark: "",
       isFetching: false
     };
   }
@@ -239,7 +233,17 @@ class RandomMachine extends React.Component {
     )
       .then((res) => {
         const fetchedQuotes = res.data.quotes
+
+        // Random number for initial states
+        let initQuote = randomNumber(fetchedQuotes);
+        let initColor = randomNumber(colors);
+        let initDarkColor = randomNumber(colorsDark);
+
         this.setState({
+          text: fetchedQuotes[initQuote].quote,
+          author: fetchedQuotes[initQuote].author,
+          color: colors[initColor],
+          colorDark: colorsDark[initDarkColor],
           fetchedQuotes
         })
       })
@@ -247,18 +251,15 @@ class RandomMachine extends React.Component {
 
   handleClick() {
     const { fetchedQuotes } = this.state;
-    const randQuote = randomNumber(quotes);
+    const randQuote = randomNumber(fetchedQuotes);
     const randColor = randomNumber(colors);
     const randDarkColor = randomNumber(colorsDark);
-    const randQ = randomNumber(fetchedQuotes);
 
-    this.setState((prevState) => ({
-      text: quotes[randQuote].quote,
-      author: quotes[randQuote].author,
+    this.setState(() => ({
+      text: fetchedQuotes[randQuote].quote,
+      author: fetchedQuotes[randQuote].author,
       color: colors[randColor],
       colorDark: colorsDark[randDarkColor],
-      newQuote: fetchedQuotes[randQ].quote,
-      newAuthor: fetchedQuotes[randQ].author
     }));
   }
 
@@ -269,41 +270,25 @@ class RandomMachine extends React.Component {
     }
 
     return (
-      <Button
-        style={appBorder}
-        id="quote-box"
-        getQuote={() => this.handleClick()}
-        color={this.state.color}
-      >
-        {/* <div id="buttons">
-          <Twitter quote={this.state.text} author={this.state.author} />
-          <Button
-            getQuote={() => this.handleClick()}
-            color={this.state.color}
-          />
-        </div> */}
-        <div id="quote-author">
-          <QuoteBox text={this.state.text} newQuote={this.state.newQuote} colorDark={this.state.colorDark} />
-          <Author author={this.state.author} newAuthor={this.state.newAuthor} colorDark={this.state.colorDark} />
+      <>
+        <div className="flex justify-center">
+          <Twitter text={this.state.text} author={this.state.author} />
         </div>
-      </Button>
+
+        <QuoteBoxButton
+          style={appBorder}
+          id="quote-box"
+          getQuote={() => this.handleClick()}
+          color={this.state.color}
+        >
+          <div id="quote-author">
+            <QuoteBox text={this.state.text} colorDark={this.state.colorDark} />
+            <Author author={this.state.author} colorDark={this.state.colorDark} />
+          </div>
+        </QuoteBoxButton>
+      </>
     );
   }
 }
-
-// export async function getServerSideProps(context) {
-
-//   const res = await axios.get(
-//     "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
-//   )
-
-//   const fetchedQuotes = res.data.quotes
-
-//   return {
-//     props: {
-//       fetchedQuotes
-//     }
-//   };
-// }
 
 export default RandomMachine;
