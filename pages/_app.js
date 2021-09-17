d421e015ccdb8b8fd402bb7f69859d99691f6473
thanapoom21 <thanapoom.p.music@gmail.com>
2021-09-16 17:00:17 -0700

    import font-awesome in main app and add brands to library

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 
+// fontawesome icons
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab)
+
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
