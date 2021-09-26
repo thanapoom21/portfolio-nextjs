1a5fd013ad4806c871337c2f4e47783fb2386d74
thanapoom21 <thanapoom.p.music@gmail.com>
2021-09-26 15:47:05 -0700

    move json object to jsonbin and fetch it instead

--- pages/collected-photons.js
+++ pages/collected-photons.js
@@ -3,101 +3,37 @@ import Layout, { siteTitle } from "../components/layout";
 import HeadingOne from "../components/headingOne";
 import utilStyles from "../styles/utils.module.css";
 import { Box, Image, Wrap, WrapItem } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
-const photos = [
-  {
-    imageUrl: "https://source.unsplash.com/user/earl_plannerzone/640x480",
-    imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4,
-  },
-  {
-    imageUrl: "https://source.unsplash.com/user/erondu/640x480",
-    imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4,
-  },
-  {
-    imageUrl: "https://source.unsplash.com/user/acharki95/640x480",
-    imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4,
-  },
-  {
-    imageUrl: "https://source.unsplash.com/user/sigmund/640x480",
-    imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4,
-  },
-  {
-    imageUrl: "https://source.unsplash.com/user/miteneva/640x480",
-    imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4,
-  },
-  {
-    imageUrl: "https://source.unsplash.com/user/tranmautritam/640x480",
-    imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4,
-  },
-  {
-    imageUrl: "https://source.unsplash.com/user/thenewmalcolm/640x480",
-    imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4,
-  },
-  {
-    imageUrl: "https://source.unsplash.com/user/louishansel/640x480",
-    imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4,
-  },
-  {
-    imageUrl: "https://source.unsplash.com/user/andrazlazic/640x480",
-    imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4,
-  },
-];
+const Photons = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-export default function Photons() {
+  useEffect(() => {
+    fetch("https://api.jsonbin.io/v3/b/614679f84a82881d6c519db1/2")
+      .then(res => res.json())
+      .then(result => {
+        setIsLoaded(true);
+        setItems(result.record.photos);
+      })
+  }, []);
+
+  const mappedImages = items.map((photo, idx) => {
+    return (
+      <WrapItem
+        key={idx}
+        w={{
+          base: "100%",
+          md: "48.55555%",
+          lg: "32.33333%",
+        }}
+        justify="space-evenly"
+      >
+        <Image src={photo.imageUrl} alt={photo.imageAlt} />
+      </WrapItem>
+    );
+  })
+
   return (
     <Layout>
       <Head>
@@ -107,25 +43,19 @@ export default function Photons() {
       <section className={`${utilStyles.headingMd}`}>
         <HeadingOne>Collected Photons</HeadingOne>
         <Box mt={4}>
-          <Wrap justify="center" align="center">
-            {photos.map((photo, idx) => {
-              return (
-                <WrapItem
-                  key={idx}
-                  w={{
-                    base: "100%",
-                    md: "48.55555%",
-                    lg: "32.33333%",
-                  }}
-                  justify="space-evenly"
-                >
-                  <Image src={photo.imageUrl} alt={photo.imageAlt} />
-                </WrapItem>
-              );
-            })}
-          </Wrap>
+          {
+            isLoaded
+              ?
+              <Wrap justify="center" align="center">
+                {mappedImages}
+              </Wrap>
+              :
+              <div>Loading.....</div>
+          }
         </Box>
       </section>
     </Layout>
   );
 }
+
+export default Photons;
