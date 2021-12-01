309d96983e9c298609bfd9fed92cef15a0e9643a
thanapoom21 <thanapoom.p.music@gmail.com>
2021-12-01 00:08:49 -0800

    add environment variables to next config

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env: {
+    MONGODB_URI: 'mongodb+srv://musii:musii@delicatespacejunk.2cspo.mongodb.net/sample_mflix?retryWrites=true&w=majority',
+    MONGODB_DB: 'sample_mflix',
+  },
+}
