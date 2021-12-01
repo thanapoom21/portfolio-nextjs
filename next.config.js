cd8eabf8191558070542379d105355108fb50ebd
thanapoom21 <thanapoom.p.music@gmail.com>
2021-12-01 00:47:32 -0800

    remove env vars from exposing to public

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    MONGODB_URI: 'mongodb+srv://musii:musii@delicatespacejunk.2cspo.mongodb.net/sample_mflix?retryWrites=true&w=majority',
-    MONGODB_DB: 'sample_mflix',
+    MONGODB_URI: process.env.MONGODB_URI,
+    MONGODB_DB: process.env.MONGODB_DB,
   },
 }
