92f5b6b6451b72ebe3f3e3f5f7e30dbf238c84f9
thanapoom21 <thanapoom.p.music@gmail.com>
2022-01-24 15:43:41 -0800

    Add next/image for SEO

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: ['jsonbin.io', 'tp-portfolio.appspot.com'],
+  },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    MONGODB_DB: process.env.MONGODB_DB,
+  },
+}
