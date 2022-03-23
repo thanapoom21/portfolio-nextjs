4ffe29eb93e234a08a2c44f0b0ecffe80add0a2c
thanapoom21 <thanapoom.p.music@gmail.com>
2022-03-22 19:13:55 -0700

    formar more file without useTabs

--- components/layout.js
+++ components/layout.js
@@ -1,37 +1,43 @@
-import Head from "next/head";
-import { Image } from "@chakra-ui/react";
-import Navbar from "./navbar";
-import Footer from "./footer";
-import styles from "../styles/layout.module.css";
-import utilStyles from "../styles/utils.module.css";
-import { AUTHOR_NAME, CURRENT_URL, HOME_OG_IMAGE_URL, SITE_DESCRIPTION, SITE_NAME, SITE_TITLE } from '../lib/constants'
+import Head from 'next/head'
+import { Image } from '@chakra-ui/react'
+import Navbar from './navbar'
+import Footer from './footer'
+import styles from '../styles/layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import {
+  AUTHOR_NAME,
+  CURRENT_URL,
+  HOME_OG_IMAGE_URL,
+  SITE_DESCRIPTION,
+  SITE_NAME,
+  SITE_TITLE,
+} from '../lib/constants'
 
 export default function Layout({ children, home }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
 
-        <meta property="og:title" content={SITE_TITLE} key="ogtitle" />
-        <meta property="og:description" content={SITE_DESCRIPTION} key="ogdesc" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={CURRENT_URL} key="ogurl" />
+        <meta property='og:title' content={SITE_TITLE} key='ogtitle' />
         <meta
-          property="og:image"
-          content={HOME_OG_IMAGE_URL}
-          key="ogimage"
+          property='og:description'
+          content={SITE_DESCRIPTION}
+          key='ogdesc'
         />
-        <meta property="og:site_name" content={SITE_NAME} key="ogsitename" />
-        <meta name="twitter:card" content="summary_large_image" key="twcard" />
-
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content={CURRENT_URL} key='ogurl' />
+        <meta property='og:image' content={HOME_OG_IMAGE_URL} key='ogimage' />
+        <meta property='og:site_name' content={SITE_NAME} key='ogsitename' />
+        <meta name='twitter:card' content='summary_large_image' key='twcard' />
       </Head>
       <div className={styles.wrapperBorder}>
         <Navbar />
-        <div className="max-w-6xl px-4 mx-auto mt-12 mb-24">
+        <div className='max-w-6xl px-4 mx-auto mt-12 mb-24'>
           {home ? (
             <header className={styles.header}>
               <Image
-                src="/images/logo_no_border2.svg"
+                src='/images/logo_no_border2.svg'
                 className={`${utilStyles.borderCircle}  w-1/2 md:w-2/4 lg:w-1/4`}
                 alt={AUTHOR_NAME}
               />
@@ -42,5 +48,5 @@ export default function Layout({ children, home }) {
         <Footer />
       </div>
     </>
-  );
+  )
 }
--- pages/scrimba-tutorial/InfoSite.js
+++ pages/scrimba-tutorial/InfoSite.js
@@ -3,10 +3,10 @@ import Navbar from './components/NavBlock'
 import Main from './components/MainBlock'
 
 export default function InfoSite() {
-	return (
-		<div id='info-site-container' className='todo-app scrimba-tutorial'>
-			<Navbar />
-			<Main />
-		</div>
-	)
+  return (
+    <div id='info-site-container' className='todo-app scrimba-tutorial'>
+      <Navbar />
+      <Main />
+    </div>
+  )
 }
