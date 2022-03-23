93b585491bfa99a5c21be44ac699cc1aae932c11
thanapoom21 <thanapoom.p.music@gmail.com>
2022-03-22 18:58:25 -0700

    debug format by different code formatters

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-	render() {
-		return (
-			<Html lang='en'>
-				<Head>
-					<link
-						rel='stylesheet'
-						href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/atom-one-dark.min.css'
-					/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link
+            rel='stylesheet'
+            href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/atom-one-dark.min.css'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
 export default MyDocument
