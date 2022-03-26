485a162a2d95468dbc7f368f094c60cb56893272
thanapoom21 <thanapoom.p.music@gmail.com>
2022-03-26 12:53:15 -0700

    format files

--- components/navbar.js
+++ components/navbar.js
@@ -1,56 +1,56 @@
-import Link from "next/link";
-import Image from "next/image";
-import { Disclosure, Transition } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import Link from 'next/link'
+import Image from 'next/image'
+import { Disclosure, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
 const navigation = [
-  { name: "Works", href: "/works", current: false },
-  { name: "Collected Photons", href: "/collected-photons", current: false },
-  { name: "Blog", href: "/blog", current: false },
-  { name: "Contact", href: "/contact", current: false },
-];
+  { name: 'Works', href: '/works', current: false },
+  { name: 'Collected Photons', href: '/collected-photons', current: false },
+  { name: 'Blog', href: '/blog', current: false },
+  { name: 'Contact', href: '/contact', current: false },
+]
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(' ')
 }
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="my-5">
+    <Disclosure as='nav' className='my-5'>
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center">
+          <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
+            <div className='relative flex items-center justify-between h-16'>
+              <div className='absolute inset-y-0 left-0 flex items-center'>
                 {/** This div was on the right displaying a bell icon for notification and the avatar */}
               </div>
-              <div className="flex-1 flex items-center justify-between sm:items-stretch sm:justify-between">
-                <div className="flex-shrink-0 flex items-center">
-                  <Link href="/">
+              <div className='flex-1 flex items-center justify-between sm:items-stretch sm:justify-between'>
+                <div className='flex-shrink-0 flex items-center'>
+                  <Link href='/'>
                     <a>
                       <Image
                         priority
-                        src="/images/logo.svg"
-                        className="block lg:block h-8 w-auto"
-                        height="50"
-                        width="50"
-                        alt="logo"
+                        src='/images/logo.svg'
+                        className='block lg:block h-8 w-auto'
+                        height='50'
+                        width='50'
+                        alt='logo'
                       />
                     </a>
                   </Link>
                 </div>
-                <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
+                <div className='hidden sm:block sm:ml-6'>
+                  <div className='flex space-x-4'>
                     {navigation.map((item) => (
                       <Link href={item.href} key={item.name}>
                         <a
                           className={classNames(
                             item.current
-                              ? "bg-gray-900 text-white"
-                              : "text-gray-500 hover:text-gray-800",
-                            "px-3 py-2 rounded-md text-sm font-medium"
+                              ? 'bg-gray-900 text-white'
+                              : 'text-gray-500 hover:text-gray-800',
+                            'px-3 py-2 rounded-md text-sm font-medium'
                           )}
-                          aria-current={item.current ? "page" : undefined}
+                          aria-current={item.current ? 'page' : undefined}
                         >
                           {item.name}
                         </a>
@@ -59,14 +59,14 @@ export default function Navbar() {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+              <div className='absolute inset-y-0 right-0 flex items-center sm:hidden'>
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
+                <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                  <span className='sr-only'>Open main menu</span>
                   {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XIcon className='block h-6 w-6' aria-hidden='true' />
                   ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    <MenuIcon className='block h-6 w-6' aria-hidden='true' />
                   )}
                 </Disclosure.Button>
               </div>
@@ -75,25 +75,25 @@ export default function Navbar() {
 
           <Transition
             show={open}
-            enter="transition-opacity duration-75"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity duration-150"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            enter='transition-opacity duration-75'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='transition-opacity duration-150'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
           >
-            <Disclosure.Panel className="sm:hidden">
-              <div className="px-2 pt-2 pb-3 space-y-1">
+            <Disclosure.Panel className='sm:hidden'>
+              <div className='px-2 pt-2 pb-3 space-y-1'>
                 {navigation.map((item) => (
                   <Link href={item.href} key={item.name}>
                     <a
                       className={classNames(
                         item.current
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-500 hover:text-gray-800",
-                        "block px-3 py-2 rounded-md text-base font-medium"
+                          ? 'bg-gray-900 text-white'
+                          : 'text-gray-500 hover:text-gray-800',
+                        'block px-3 py-2 rounded-md text-base font-medium'
                       )}
-                      aria-current={item.current ? "page" : undefined}
+                      aria-current={item.current ? 'page' : undefined}
                     >
                       {item.name}
                     </a>
@@ -105,5 +105,5 @@ export default function Navbar() {
         </>
       )}
     </Disclosure>
-  );
+  )
 }
--- pages/collected-photons.js
+++ pages/collected-photons.js
@@ -1,43 +1,44 @@
-import Head from "next/head";
-import Image from 'next/image';
-import Layout from "../components/layout";
-import HeadingOne from "../components/headingOne";
-import utilStyles from "../styles/utils.module.css";
-import { Box, Wrap, WrapItem } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '../components/layout'
+import HeadingOne from '../components/headingOne'
+import utilStyles from '../styles/utils.module.css'
+import { Box, Wrap, WrapItem } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 import { SITE_TITLE } from '../lib/constants'
 
 const Photons = () => {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [items, setItems] = useState([])
 
   useEffect(() => {
-    fetch("https://api.jsonbin.io/v3/b/614679f84a82881d6c519db1/2")
-      .then(res => res.json())
-      .then(result => {
-        setIsLoaded(true);
-        setItems(result.record.photos);
+    fetch('https://api.jsonbin.io/v3/b/614679f84a82881d6c519db1/2')
+      .then((res) => res.json())
+      .then((result) => {
+        setIsLoaded(true)
+        setItems(result.record.photos)
       })
-  }, []);
+  }, [])
 
   const mappedImages = items.map((photo, idx) => {
     return (
       <WrapItem
         key={idx}
         w={{
-          base: "100%",
-          md: "48.55555%",
-          lg: "32.33333%",
+          base: '100%',
+          md: '48.55555%',
+          lg: '32.33333%',
         }}
-        justify="space-evenly"
+        justify='space-evenly'
       >
         <Image
           src={photo.imageUrl}
-          width="480"
-          height="320"
-          alt={photo.imageAlt} />
+          width='480'
+          height='320'
+          alt={photo.imageAlt}
+        />
       </WrapItem>
-    );
+    )
   })
 
   return (
@@ -49,19 +50,17 @@ const Photons = () => {
       <section className={`${utilStyles.headingMd}`}>
         <HeadingOne>Collected Photons</HeadingOne>
         <Box mt={4}>
-          {
-            isLoaded
-              ?
-              <Wrap justify="center" align="center">
-                {mappedImages}
-              </Wrap>
-              :
-              <div>Loading.....</div>
-          }
+          {isLoaded ? (
+            <Wrap justify='center' align='center'>
+              {mappedImages}
+            </Wrap>
+          ) : (
+            <div>Loading.....</div>
+          )}
         </Box>
       </section>
     </Layout>
-  );
+  )
 }
 
-export default Photons;
+export default Photons
