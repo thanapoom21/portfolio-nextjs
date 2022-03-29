26777c34cd44c3e0030d8b6466a773eaa4058fb5
thanapoom21 <thanapoom.p.music@gmail.com>
2022-03-29 14:45:36 -0700

    create vote button and keep tracks of points

--- pages/fullstackopen-tutorial/Anecdotes.js
+++ pages/fullstackopen-tutorial/Anecdotes.js
@@ -1,31 +1,52 @@
 import { useState } from 'react'
 import { Button } from '@chakra-ui/react'
-const Anecdotes = () => {
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients',
-  ]
-  const [selected, setSelected] = useState(0)
 
-  const randomize = (anecdotes) => {
-    return Math.round(Math.random() * anecdotes.length)
-  }
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients',
+]
+
+const randomize = (array) => {
+  return Math.round(Math.random() * (array.length - 1))
+}
+
+const Anecdotes = () => {
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+  const mostVotes = Math.max(...points)
 
   const handleClick = () => {
     const selectedAnecdote = randomize(anecdotes)
     setSelected(selectedAnecdote)
   }
 
+  const handleVote = () => {
+    const copiedPoints = [...points]
+    copiedPoints[selected] += 1
+    setPoints(copiedPoints)
+  }
+
   return (
     <div>
       <p className='text-xl md:text-2xl font-bold tracking-tight my-2'>
-        Random Anecdotes
+        Anecdotes of The Day
       </p>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} vote</p>
+      <Button
+        mr={2}
+        colorScheme='blackAlpha'
+        variant='outline'
+        borderRadius='1px'
+        onClick={handleVote}
+      >
+        Vote
+      </Button>
       <Button
         mr={2}
         colorScheme='blackAlpha'
@@ -35,7 +56,12 @@ const Anecdotes = () => {
       >
         Next Anecdotes
       </Button>
-      <p>{anecdotes[selected]}</p>
+
+      <p className='text-xl md:text-2xl font-bold tracking-tight my-2'>
+        Anecdote with The Most Votes
+      </p>
+      {anecdotes[points.indexOf(mostVotes)]}
+      <p>has {mostVotes} vote</p>
     </div>
   )
 }
