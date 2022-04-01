5aa2ca2bb91ea7728f0b4c436ca915fa2761403a
thanapoom21 <thanapoom.p.music@gmail.com>
2022-04-01 15:19:56 -0700

    implement search filter for a specific value

--- pages/fullstackopen-tutorial/Phonebook.js
+++ pages/fullstackopen-tutorial/Phonebook.js
@@ -2,49 +2,62 @@ import { useState } from "react"
 import { Button } from '@chakra-ui/react'
 
 const Phonebook = () => {
-  const [persons, setPersons] = useState([
+  const [people, setPeople] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [filteredPeople, setFilteredPeople] = useState([])
+
+  const [name, setName] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [searchedWord, setSearchedWord] = useState('')
+  const filteredWord = !searchedWord ? people : people.filter(person => person.name.toLowerCase().includes(searchedWord.toLowerCase()))
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    alert(`${newName} and ${newNumber} are already added to phonebook`)
+    alert(`${name} and ${phoneNumber} are already added to phonebook`)
     const personObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1
+      name: name,
+      number: phoneNumber,
+      id: people.length + 1
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    setPeople(people.concat(personObject))
+    setName('')
+    setPhoneNumber('')
   }
 
   const handleChangeName = (event) => {
-    setNewName(event.target.value)
+    setName(event.target.value)
   }
 
   const handleChangeNumber = (event) => {
-    setNewNumber(event.target.value)
+    setPhoneNumber(event.target.value)
+  }
+
+  const handleSearch = (event) => {
+    setSearchedWord(event.target.value)
+    setFilteredPeople(filteredWord)
   }
 
   return (
     <div>
       <p className='text-2xl md:text-4xl font-bold tracking-tight my-2'>Phonebook Exercise</p>
       <p className='text-xl md:text-2xl font-bold tracking-tight my-2'>Phonebook</p>
+      <div className="my-2">
+        Search: <input value={searchedWord} onChange={handleSearch} placeholder='type a word' />
+      </div>
+
       <form onSubmit={handleSubmit}>
         <div className="my-2">
-          Name: <input value={newName} onChange={handleChangeName} placeholder='your name' />
+          Name: <input value={name} onChange={handleChangeName} placeholder='your name' />
         </div>
 
         <div className="my-2">
-          Number: <input value={newNumber} onChange={handleChangeNumber} placeholder='your phone number' />
+          Number: <input value={phoneNumber} onChange={handleChangeNumber} placeholder='your phone number' />
         </div>
         <Button
           mr={2}
@@ -57,7 +70,13 @@ const Phonebook = () => {
         </Button>
       </form>
       <p className='text-xl md:text-2xl font-bold tracking-tight my-2'>Numbers</p>
-      {persons.map(p => <p key={p.number}>{`${p.id}. ${p.name} & ${p.number}`}</p>)}
+      {
+        !searchedWord
+          ?
+          people.map(person => <p key={person.id}>{`${person.name} - ${person.number}`}</p>)
+          :
+          filteredWord.map(person => <p key={person.id}>{`${person.name} - ${person.number}`}</p>)
+      }
     </div>
   )
 }
