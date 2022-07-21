830aba7d989204d99cfb589abc63e677e12f9c75
thanapoom21 <thanapoom.p.music@gmail.com>
2022-07-20 22:38:03 -0700

    add quicksand font

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,16 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/atom-one-dark.min.css"
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         <body>
           <Main />
