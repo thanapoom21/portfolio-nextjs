5e3486fe77449cbd84ad9377f9caf830f00ae7c3
thanapoom21 <thanapoom.p.music@gmail.com>
2022-08-04 23:58:29 -0700

    increase container size

--- components/post-body.js
+++ components/post-body.js
@@ -3,7 +3,7 @@ import markdownStyles from "../styles/markdown-styles.module.css";
 
 export default function PostBody({ content }) {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-4xl mx-auto">
       <div
         className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
--- components/post-header.js
+++ components/post-header.js
@@ -13,7 +13,7 @@ export default function PostHeader({ title, coverImage, date, author }) {
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} height={620} width={1240} />
       </div>
-      <div className="max-w-2xl mx-auto">
+      <div className="max-w-4xl mx-auto">
         <div className="block md:hidden mb-6">
           <Avatar name={author.name} picture={author.picture} />
         </div>
